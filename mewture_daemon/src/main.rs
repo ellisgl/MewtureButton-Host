dbf2f1d6ca092e57d1f28a79da2452db02aa2698
EllisGL <xellisx@gmail.com>
2023-06-19 20:33:18 -0500

    Slowly getting this code broken out and simplified, so it can be unit tested.

--- mewture_daemon/src/main.rs
+++ mewture_daemon/src/main.rs
@@ -57,160 +57,49 @@ fn main() -> Result<(), Box<dyn Error>> {
         println!("Initial mute state: {:?}", current_mute_state);
     }
 
-    let mut received_buffer: Vec<u8> = vec![0; 64];
-    loop {
-        let bytes_read = match port.read(&mut received_buffer) {
-            Ok(bytes_read) => bytes_read,
-            Err(_e) => { 0 }
-        };
+    run(config, &mut pa, &mut port, &mut current_mute_state, cli.debug)
+}
 
-        if bytes_read > 7 {
-            // Parse the received data.
-            let message = ddaa_protocol::parse_protocol_message(&mut received_buffer);
-            if cli.debug {
-                // Print the incoming message if debug is enabled.
-                println!("Incoming message: {:?}", message);
-            }
+/// Check if the source's mute state has changed.
+fn check_for_mute_state_change(
+    config: &mewture_shared::Config,
+    pa: &mut PulseAudio,
+    port: &mut Box<dyn SerialPort>,
+    current_mute_state: &mut bool, debug: bool
+) -> Result<(), Box<dyn Error>> {
+    // Check if the source mute state has changed.
+    let new_mute_state = get_source_mute(&pa, &config)?;
+    let mut message = ProtocolMessage {
+        message_type: MessageType::Request,
+        command: ddaa_protocol::Command::Write,
+        variable: 0x00,
+        data: vec![0x00]
+    };
 
-            if let Some(parsed_message) = message {
-                if parsed_message.message_type == MessageType::Request {
-                    if cli.debug {
-                        println!("Received request: {:?}", parsed_message);
-                    }
-
-                    match parsed_message.command {
-                        ddaa_protocol::Command::Ping => {
-                            // Received ping, response to our caller.
-                            if cli.debug {
-                                println!("Received ping");
-                            }
-
-                            respond_to_ping(&mut port, parsed_message);
-                        }
-                        ddaa_protocol::Command::Read => {
-                            // Received read request.
-                            if cli.debug {
-                                println!("Received read request");
-                            }
-
-                            if parsed_message.variable == 0x00 {
-                                // Respond with current mute state.
-                                if cli.debug {
-                                    println!("Received mute read request");
-                                }
-
-                                let buffer = ddaa_protocol::create_protocol_buffer(
-                                    MessageType::ResponseSuccess,
-                                    ddaa_protocol::Command::Read,
-                                    parsed_message.variable,
-                                    &[u8::from(current_mute_state)]
-                                );
-
-                                match port.write(&buffer) {
-                                    Ok(size) => {
-                                        if cli.debug {
-                                            println!("Wrote {} bytes", size);
-                                            println!("Wrote: {:?}", buffer);
-                                        }
-                                    }
-                                    Err(_e) => {
-                                        eprintln!("Error writing to serial port");
-                                    }
-                                }
-                            }
-                        }
-                        ddaa_protocol::Command::Write => {
-                            // Received write request.
-                            if cli.debug {
-                                println!("Received write request");
-                            }
-
-                            if parsed_message.variable == 0x00 {
-                                if cli.debug {
-                                    println!("Received mute write request");
-                                }
-
-                                match parsed_message.data[0] {
-                                    0x00 => {
-                                        // Received mute request.
-                                        // Set source mute state to true.
-                                        if cli.debug {
-                                            println!("Received mute set to true request");
-                                        }
-
-                                        set_source_mute(&pa, &config, false, cli.debug)?;
-                                        write_message_to_port(&mut port, MessageType::ResponseSuccess, parsed_message, cli.debug)?;
-                                    }
-                                    0x01 => {
-                                        // Received unmute request.
-                                        // Set source mute state to false.
-                                        if cli.debug {
-                                            println!("Received mute set to false request");
-                                        }
-
-                                        set_source_mute(&pa, &config, true, cli.debug)?;
-                                        write_message_to_port(&mut port, MessageType::ResponseSuccess, parsed_message, cli.debug)?;
-                                    }
-                                    0x02 => {
-                                        // Received invert mute request.
-                                        if cli.debug {
-                                            println!("Received mute set to invert request");
-                                        }
-
-                                        invert_source_mute(&mut pa, &config, &mut port, &parsed_message, current_mute_state, cli.debug)?;
-                                        write_message_to_port(&mut port, MessageType::ResponseSuccess, parsed_message, cli.debug)?;
-
-                                    }
-                                   _ => {
-                                       // Data for mute variable is invalid.
-                                       eprintln!("Received unknown value:{:?}", parsed_message.data[0]);
-                                       write_message_to_port(&mut port, MessageType::ResponseError, parsed_message, cli.debug)?;
-                                    }
-                                }
-                            }
-                        }
-                    }
+    if new_mute_state != *current_mute_state {
+        message.data[0] = u8::from(new_mute_state);
+        return match write_message_to_port(
+            port,
+            MessageType::Request,
+            message,
+            debug
+        ) {
+            Ok(_) => {
+                if debug {
+                    println!("Setting mute state variable to {:?}", new_mute_state);
                 }
+
+                *current_mute_state = new_mute_state;
+                Ok(())
+            },
+            Err(e) => {
+                eprintln!("Error writing to serial port: {}", e);
+                Err(e.into())
             }
-        } else if bytes_read > 0 && bytes_read <= 7 {
-            // We could handle this, but we can just ignore and continue for now.
-        }
-
-        // Check if the source mute state has changed.
-        let new_mute_state = get_source_mute(&pa, &config)?;
-        let mut message = ProtocolMessage {
-            message_type: MessageType::Request,
-            command: ddaa_protocol::Command::Write,
-            variable: 0x00,
-            data: vec![0x00]
         };
-
-        if new_mute_state != current_mute_state {
-            message.data[0] = u8::from(new_mute_state);
-            match write_message_to_port(
-                &mut port,
-                MessageType::Request,
-                message,
-                cli.debug
-            ) {
-                Ok(_) => {
-                    if cli.debug {
-                        println!("Setting mute state variable to {:?}", new_mute_state);
-                    }
-
-                    current_mute_state = new_mute_state;
-                },
-                Err(e) => {
-                    eprintln!("Error writing to serial port: {}", e);
-                    exit(1);
-                }
-            };
-        }
-
-        // Clear the buffer.
-        received_buffer.clear();
-        received_buffer.resize(64, 0);
     }
+
+    Ok(())
 }
 
 /// Get the configuration.
@@ -232,6 +121,17 @@ fn get_config(path: PathBuf) -> Result<mewture_shared::Config, Box<dyn Error>> {
                     e
                 ).into()
             )
+    }
+}
+
+/// Get the source mute state.
+fn get_source_mute(pa: &PulseAudio, config: &mewture_shared::Config) -> Result<bool, Box<dyn Error>> {
+    match pa.get_source_mute(PAIdent::Index(config.audio_device_index)) {
+        Ok(m) => Ok(m),
+        Err(e) => {
+            eprintln!("Error getting mute state: {}", e);
+            Err(e.into())
+        }
     }
 }
 
@@ -280,6 +180,143 @@ fn invert_source_mute(
     }
 }
 
+/// Handle a read request.
+fn handle_read_request(
+    port: &mut Box<dyn SerialPort>,
+    parsed_message: ProtocolMessage,
+    current_mute_state: &bool,
+    debug: bool
+) -> Result<(), Box<dyn Error>> {
+    // Received read request.
+    if debug {
+        println!("Received read request");
+    }
+
+    if parsed_message.variable == 0x00 {
+        // Respond with current mute state.
+        if debug {
+            println!("Received mute read request");
+        }
+
+        let buffer = ddaa_protocol::create_protocol_buffer(
+            MessageType::ResponseSuccess,
+            ddaa_protocol::Command::Read,
+            parsed_message.variable,
+            &[u8::from(*current_mute_state)]
+        );
+
+        match port.write(&buffer) {
+            Ok(size) => {
+                if debug {
+                    println!("Wrote {} bytes", size);
+                    println!("Wrote: {:?}", buffer);
+                }
+            }
+            Err(_e) => {
+                eprintln!("Error writing to serial port");
+            }
+        }
+    }
+
+    // Should probably do some sort of error here, but for now just continue on.
+    Ok(())
+}
+
+/// Handle a request.
+fn handle_request(
+    config: &mewture_shared::Config,
+    pa: &mut PulseAudio,
+    port: &mut Box<dyn SerialPort>,
+    parsed_message: ProtocolMessage,
+    current_mute_state: &mut bool,
+    debug: bool
+) -> Result<(), Box<dyn Error>> {
+    if debug {
+        println!("Received request: {:?}", parsed_message);
+    }
+
+    match parsed_message.command {
+        ddaa_protocol::Command::Ping => {
+            // Received ping, response to our caller.
+            if debug {
+                println!("Received ping");
+            }
+
+            respond_to_ping(port, parsed_message);
+        }
+        ddaa_protocol::Command::Read => {
+            // Received read request.
+            handle_read_request(port, parsed_message, current_mute_state, debug)?
+        }
+        ddaa_protocol::Command::Write => {
+            // Received write request.
+            handle_write_request(config, pa, port, parsed_message, current_mute_state, debug)?
+        }
+    }
+
+    Ok(())
+}
+
+/// Handle a write request.
+fn handle_write_request(
+    config: &mewture_shared::Config,
+    pa: &mut PulseAudio,
+    port: &mut Box<dyn SerialPort>,
+    parsed_message: ProtocolMessage,
+    current_mute_state: &mut bool,
+    debug: bool
+) -> Result<(), Box<dyn Error>> {
+    if debug {
+        println!("Received write request: {:?}", parsed_message);
+    }
+
+    if parsed_message.variable == 0x00 {
+        if debug {
+            println!("Received mute write request");
+        }
+
+        match parsed_message.data[0] {
+            0x00 => {
+                // Received mute request.
+                // Set source mute state to true.
+                if debug {
+                    println!("Received mute set to true request");
+                }
+
+                set_source_mute(pa, &config, false, debug)?;
+                write_message_to_port(port, MessageType::ResponseSuccess, parsed_message, debug)?;
+            }
+            0x01 => {
+                // Received unmute request.
+                // Set source mute state to false.
+                if debug {
+                    println!("Received mute set to false request");
+                }
+
+                set_source_mute(pa, &config, true, debug)?;
+                write_message_to_port(port, MessageType::ResponseSuccess, parsed_message, debug)?;
+            }
+            0x02 => {
+                // Received invert mute request.
+                if debug {
+                    println!("Received mute set to invert request");
+                }
+
+                invert_source_mute(pa, &config, port, &parsed_message, *current_mute_state, debug)?;
+                write_message_to_port(port, MessageType::ResponseSuccess, parsed_message, debug)?;
+
+            }
+            _ => {
+                // Data for mute variable is invalid.
+                eprintln!("Received unknown value: {:?}", parsed_message.data[0]);
+                write_message_to_port(port, MessageType::ResponseError, parsed_message, debug)?;
+            }
+        }
+    }
+
+    Ok(())
+}
+
 /// Respond to a ping message.
 fn respond_to_ping(port: &mut Box<dyn SerialPort>, message: ProtocolMessage) {
     match port.write(&ddaa_protocol::create_protocol_buffer(
@@ -295,6 +332,84 @@ fn respond_to_ping(port: &mut Box<dyn SerialPort>, message: ProtocolMessage) {
             exit(1);
         }
     };
+}
+
+/// The loop that does all the things for the daemon.
+fn run(
+    config: mewture_shared::Config,
+    pa: &mut PulseAudio,
+    port: &mut Box<dyn SerialPort>,
+    current_mute_state: &mut bool,
+    debug: bool
+) -> Result<(), Box<dyn Error>> {
+    let mut received_buffer: Vec<u8> = vec![0; 64];
+    loop {
+        let bytes_read = match port.read(&mut received_buffer) {
+            Ok(bytes_read) => bytes_read,
+            Err(_e) => { 0 }
+        };
+
+        if bytes_read > 7 {
+            // Parse the received data.
+            let message = ddaa_protocol::parse_protocol_message(&mut received_buffer);
+            if debug {
+                // Print the incoming message if debug is enabled.
+                println!("Incoming message: {:?}", message);
+            }
+
+            if let Some(parsed_message) = message {
+                if parsed_message.message_type == MessageType::Request {
+                    if debug {
+                        println!("Received request: {:?}", parsed_message);
+                    }
+
+                    handle_request(
+                        &config,
+                        pa,
+                        port,
+                        parsed_message,
+                        current_mute_state,
+                        debug
+                    )?
+                }
+            }
+        } else if bytes_read > 0 && bytes_read <= 7 {
+            // We could handle this, but we can just ignore and continue for now.
+        }
+
+        // Check if the source mute state has changed.
+        check_for_mute_state_change(&config, pa, port, current_mute_state, debug)?;
+
+        // Clear the buffer.
+        received_buffer.clear();
+        received_buffer.resize(64, 0);
+    }
+}
+
+/// Set the source mute state.
+fn set_source_mute(
+    pa: &PulseAudio,
+    config: &mewture_shared::Config,
+    mute_state: bool,
+    debug: bool
+) -> Result<(), Box<dyn Error>> {
+    if debug {
+        println!("Setting source mute state to {:?}", mute_state);
+    }
+
+    match pa.set_source_mute(PAIdent::Index(config.audio_device_index), mute_state) {
+        Ok(res) => {
+            if debug {
+                println!("pa.set_source_mute responded with {:?}", res);
+            }
+
+            Ok(())
+        },
+        Err(e) => {
+            eprintln!("Error setting mute state: {}", e);
+            Err(e.into())
+        }
+    }
 }
 
 /// Set the default source device in PulseAudio.
@@ -327,43 +442,6 @@ fn setup_serial_port(config: &mewture_shared::Config) -> Result<Box<dyn SerialPo
         Ok(p) => Ok(p),
         Err(e) => {
             eprintln!("Error reading from serial port: {}", e);
-            Err(e.into())
-        }
-    }
-}
-
-/// Get the source mute state.
-fn get_source_mute(pa: &PulseAudio, config: &mewture_shared::Config) -> Result<bool, Box<dyn Error>> {
-    match pa.get_source_mute(PAIdent::Index(config.audio_device_index)) {
-        Ok(m) => Ok(m),
-        Err(e) => {
-            eprintln!("Error getting mute state: {}", e);
-            Err(e.into())
-        }
-    }
-}
-
-/// Set the source mute state.
-fn set_source_mute(
-    pa: &PulseAudio,
-    config: &mewture_shared::Config,
-    mute_state: bool,
-    debug: bool
-) -> Result<(), Box<dyn Error>> {
-    if debug {
-        println!("Setting source mute state to {:?}", mute_state);
-    }
-
-    match pa.set_source_mute(PAIdent::Index(config.audio_device_index), mute_state) {
-        Ok(res) => {
-            if debug {
-                println!("pa.set_source_mute responded with {:?}", res);
-            }
-
-            Ok(())
-        },
-        Err(e) => {
-            eprintln!("Error setting mute state: {}", e);
             Err(e.into())
         }
     }
